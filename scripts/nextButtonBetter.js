bed8b36b063ec7f4d9fd275e095512dd514732c0
instagr4h4m <gh1315@nyu.edu>
2021-01-22 12:28:12 -0500

    Added loadJSON

--- scripts/nextButtonBetter.js
+++ scripts/nextButtonBetter.js
@@ -1,7 +1,21 @@
 // File dictates button usage
 
+// Code below from https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
+function loadJSON(callback) {   
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'data/buttons.json', true); 
+  xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+          // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+          callback(xobj.responseText);
+        }
+  };
+  xobj.send(null);  
+}
+
 // Assign JSON, load load first cell
-let cellData = NULL; 
+let cellData = null; 
 loadJson( (jsonObject) => { 
   cellData = jsonObject; 
   displayCell(1); 
